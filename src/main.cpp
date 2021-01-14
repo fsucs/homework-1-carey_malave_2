6c60c22800a90c7c039f8e36feceb268398bff15
Kathryn Carey <kmc19k@my.fsu.edu>
2021-01-14 10:39:27 -0500

    Fixing max count

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,14 @@ using namespace std;
 int get_max_count(string sinput){
 	int max = 0; //returns back to main. max number of a repeated character
 	int count[100] = {}; //array of each character's count
+	int match = 0;
 
 	for (int x = 0; x < sinput.size(); x++){
-		if (x == 0)//first character receives 1
-			count[x]++;
+		if (x == 0){//first character receives 1
+			count[x]++;}
 		else{
-			int match = 0; //to see if the last character receives a count
-			for (int y = x; y < x; y++){
+			match = 0;
+			for (int y = x; y < sinput.size()-1; y++){
 				if (sinput[y] == sinput[x]){
 					count[y]++;
 					match++;
@@ -36,7 +37,7 @@ int get_max_count(string sinput){
 		if (match > 0) //if the last character does receive a count
 			count[x]++;
 	} 
-	for (int i = 0; i < sinput.length(); i++){
+	for (int i = 0; i < count[i]; i++){
 		if (count[i] > max)
 			max = count[i];
 	}
